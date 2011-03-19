7515963066a7764793324e93bb2dd2703f00ee1c
Attila Olah <me@aatiis.me>
2011-03-19 14:20:04 +0100

    rm battery monitor widget

--- downloads/lua/rc.lua
+++ downloads/lua/rc.lua
@@ -59,36 +59,6 @@ function hex(inp)
 end
 -- }}}
 
--- {{{ Battery monitor
-mybattmon = widget({ type = "textbox", name = "mybattmon", align = "right" })
-function battery_status ()
-    local output={} -- output buffer
-    local fd=io.popen("acpitool -b", "r") -- list present batteries
-    local line=fd:read()
-    while line do -- there might be several batteries
-        local battery_num = string.match(line, "Battery \#(%d+)")
-        local battery_load = string.match(line, " (%d*)\.%d+%%")
-        local time_rem = string.match(line, "(%d+\:%d+)\:%d+")
-        if battery_num and battery_load and time_rem then
-            table.insert(output, "<span color=\"#"
-                .. hex(170 * (100 - tonumber(battery_load)) / 100)
-                .. hex(170 * tonumber(battery_load) / 100)
-                .. "00\">" .. time_rem .. " " .. battery_load .. "%</span>")
-        elseif battery_num and battery_load then -- remaining time unavailable
-            table.insert(output, "<span color=\"#00AA00\">" .. battery_load.."%</span>")
-        end
-        line=fd:read() -- read next line
-    end
-    return table.concat(output," ")
-end
-mybattmon.text = " " .. battery_status() .. " "
-my_battmon_timer=timer({timeout=17})
-my_battmon_timer:add_signal("timeout", function()
-    mybattmon.text = " " .. battery_status() .. " "
-end)
-my_battmon_timer:start()
--- }}}
-
 -- {{{ Heat monitor
 myheatmon = widget({ type = "textbox", name = "myheatmon", align = "right" })
 function heat_status ()
