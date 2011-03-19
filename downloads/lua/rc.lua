4ddade81c72fe0a2eec928dce4289e158a175b89
Attila Olah <me@aatiis.me>
2011-03-19 14:19:17 +0100

    compositing, better heat monitoring widget, etc.

--- downloads/lua/rc.lua
+++ downloads/lua/rc.lua
@@ -92,18 +92,15 @@ my_battmon_timer:start()
 myheatmon = widget({ type = "textbox", name = "myheatmon", align = "right" })
 function heat_status ()
     local output = {} -- output buffer
-    local fd = io.popen("acpitool -t", "r") -- list present thermal zones
+    local fd = io.popen("cat /sys/class/thermal/thermal_zone0/temp", "r") -- cat first thermal zone
     local line = fd:read()
-    while line do -- there might be several thermal zones
-        local heat_num = string.match(line, "Thermal zone (%d+)")
-        local heat_load = string.match(line, "(%d+) C")
-        if heat_num and heat_load then
-            table.insert(output, "<span color=\"#"
-                .. hex(255 * tonumber(heat_load) / 105)
-                .. hex(255 * (105 - tonumber(heat_load)) / 105)
-                .. "00\">" .. heat_load .. "&#8451;</span>")
-        end
-        line=fd:read() -- read next line
+    local heat = string.match(line, "(%d+)")
+    if heat then
+        heat = tonumber(heat) / 1000
+        table.insert(output, "<span color=\"#"
+            .. hex(255 * (heat - 40) / 65)
+            .. hex(255 * (105 - heat) / 65)
+            .. "00\">" .. heat .. "&#8451;</span>")
     end
     return table.concat(output," ")
 end
@@ -400,3 +397,8 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Startup scripts:
+awful.util.spawn_with_shell("nautilus -n")
+awful.util.spawn_with_shell("xcompmgr -c -C -f -F -D 2.5 -l -2 -t -2 -r 2 -o 0.25")
+awful.util.spawn_with_shell("gnome-power-manager")
