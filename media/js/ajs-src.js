cfb99905b3895346b98b03ef41e890e5007357f5
Attila Oláh <me@aatiis.me>
2011-06-03 13:33:13 +0200

    +1 button

--- media/js/ajs-src.js
+++ media/js/ajs-src.js
@@ -156,6 +156,7 @@
         asynchronous_javascript_loader('http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js');
         asynchronous_javascript_loader('http://connect.facebook.net/en_US/all.js');
         asynchronous_javascript_loader('http://www.google-analytics.com/ga.js');
+        asynchronous_javascript_loader('http://apis.google.com/js/plusone.js');
         asynchronous_javascript_loader('/media/js/jquery-treeview-src.js');
         asynchronous_javascript_loader('/media/js/data-src.js');
         asynchronous_javascript_loader('/media/js/tween-src.js');
